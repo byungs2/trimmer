bf0339a53acf6340e2c3d489f4d217de8581975a
Byunghun <byunghun.lee@fainders.ai>
2021-12-07 11:44:39 +0900

    ttt

--- trimming.py
+++ trimming.py
@@ -1,5 +1,6 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.editor import VideoFileClip
+import cv2
 import json
 import os
 
@@ -18,6 +19,8 @@ class VideoClip:
         self.origin_file_path = origin;
         self.dest = dest;
         self.duration = 0.0;
+        self.fps = 0.0;
+        self.dropped_sec = 0.0;
     def calculate(self):
         self.rtp_diff_sec = (self.rtcp_rtp - self.rtp)/90000.0;
         self.start_abs_time = (self.rtcp >> 32) - self.rtp_diff_sec;
@@ -30,6 +33,8 @@ class VideoClip:
         clip = VideoFileClip(self.origin_file_path);
         self.duration = clip.duration;
         print('%0.20f' % self.duration, self.origin_file_path);
+    def set_fps(self, fps):
+        self.fps = fps;
 
 class VideoSinker:
     def __init__(self, video_list):
@@ -43,11 +48,21 @@ class VideoSinker:
         for i in self.video_list:
             i.dynamic_offset = self.max_diff - i.real_time;
             i.offset = i.dynamic_offset + i.fixed_offset;
-            print('%0.20f' % i.offset, i.origin_file_path);
+            print('%0.20f' % (i.offset - i.dropped_sec), i.dropped_sec, i.origin_file_path);
 
-    def trimming_video_by_lock_step(self):
+    def trimming_video_by_lock_step(self, hard_offset):
+        index = 0;
         for i in self.video_list:
-            ffmpeg_extract_subclip(i.origin_file_path, i.offset, i.duration, targetname=i.dest);
+            if(i.offset - i.dropped_sec - i.fixed_offset) > 0 and index != 4 :
+                ffmpeg_extract_subclip(i.origin_file_path, float(i.offset - i.dropped_sec), float(i.duration), targetname=i.dest);
+            elif (index == 4) :
+                print("Re-encode process");
+                clip = VideoFileClip(i.origin_file_path);
+                newclip= clip.subclip(i.offset + hard_offset/i.fps, int(i.duration));
+                newclip.write_videofile(i.dest, fps=i.fps);
+            else :
+                ffmpeg_extract_subclip(i.origin_file_path, float(i.offset), float(i.duration), targetname=i.dest);
+            index+=1;
 
 class JsonReader:
     def __init__(self, json_file_path):
@@ -88,7 +103,26 @@ def filtering_and_sorting(video_f_list, json_f_list):
         j_tmp[i].sort();
     return v_tmp, j_tmp;
 
-def video_list_maker(video_list, v, j):
+def calculate_dropped_frame(clip, threshold, fps, rtp_list) :
+    before = 0.0;
+    current = 0.0;
+    diff = 0.0;
+    dropped = 0.0;
+    step = 90000.0/fps;
+    odd_cnt = 0;
+    init = 0.0;
+    for i in rtp_list :
+        if(odd_cnt%2 == 1):
+            current = i;
+            diff = current - before;
+            if (diff > step * threshold) and init != 0.0 :
+                dropped += (diff/90000.0);
+            before = current;
+            init = 1.0;
+        odd_cnt += 1;
+    clip.dropped_sec = dropped;
+
+def video_list_maker(video_list, v, j, json_dir_list, video_dir_list):
     pre_fix = "./cutted/cutted_"
 
     for i in range(len(j)):
@@ -109,16 +143,31 @@ def video_list_maker(video_list, v, j):
                 clip = VideoClip(rtcp_json_reader.json_data["rtcp_packets"][0], 
                         rtcp_json_reader.json_data["rtcp_packets"][1], 
                         rtp_json_reader.json_data["rtp_packets"][1], 
-                        12.0, 
-                        video_dir_list[i] + "/" + v[i][k/2], 
-                        "./cutted/cutted_" + str(i) + "/" + v[i][k/2]);
+                        10.0, 
+                        video_dir_list[i] + "/" + v[i][int(k/2)], 
+                        "./cutted/cutted_" + str(i) + "/" + v[i][int(k/2)]);
                 v_list.append(clip);
+                cv_clip = cv2.VideoCapture(clip.origin_file_path);
+                fps_cv = cv_clip.get(cv2.CAP_PROP_FPS);
+                clip.set_fps(fps_cv);
+                calculate_dropped_frame(clip, 1.4, fps_cv, rtp_json_reader.json_data["rtp_packets"]);
         video_list.append(v_list);
 
        
 if __name__ == "__main__": 
-    video_dir_list = ["/data/_rotated_azure_1", "/data/_rotated_azure_2", "/data/_rotated_azure_3", "/data/_usb_normal", "/data/_reo_241"];
-    json_dir_list = ["/data/_azure_1", "/data/_azure_2", "/data/_azure_3", "/data/_usb_normal", "/data/_reo_241"];
+    # unit :: frame
+    hard_offset_index = 0;
+    hard_offset = [19.0, 19.0, 36.0, 67.0, 35.0, 46.0, 62.0, 30.0, 50.0, 12.0, 61.0, 31.0, 43.0, 56.0, 39.0, 75.0, 51.0];
+    
+    #video_dir_list = ["/data/_rotated_azure_1", "/data/_rotated_azure_2", "/data/_rotated_azure_3", "/data/_usb_normal", "/data/_reo_241"];
+    #json_dir_list = ["/data/_azure_1", "/data/_azure_2", "/data/_azure_3", "/data/_usb_normal", "/data/_reo_241"];
+     
+    video_dir_list = [ "./origin/lanzhou_azure_3", "./origin/lanzhou_reo_241", "./origin/lanzhou_reo_242"];
+    json_dir_list = [ "./origin/lanzhou_azure_3", "./origin/lanzhou_reo_241", "./origin/lanzhou_reo_242"];
+
+    #video_dir_list = ["./origin/_azure_1", "./origin/_azure_2", "./origin/_azure_3", "./origin/_usb_normal", "./origin/_reo_241"];
+    #json_dir_list = ["./origin/_azure_1", "./origin/_azure_2", "./origin/_azure_3", "./origin/_usb_normal", "./origin/_reo_241"];
+
     video_filename_list = [];
     json_filename_list = [];
     get_file_names(video_dir_list, json_dir_list, video_filename_list, json_filename_list);
@@ -127,14 +176,32 @@ if __name__ == "__main__":
 
     video_list = [];
 
-    video_list_maker(video_list, v, j);
+    video_list_maker(video_list, v, j, json_dir_list, video_dir_list);
 
     for i in range(len(v[0])):
-        video_list[0][i].calculate();
-        video_list[1][i].calculate();
-        video_list[2][i].calculate();
-        video_list[3][i].calculate();
-        video_list[4][i].calculate();
-        video_sinker = VideoSinker([video_list[0][i], video_list[1][i], video_list[2][i], video_list[3][i], video_list[4][i]]);
+        instance_video_list = [];
+        for j in range(len(video_list)):
+            instance_video_list.append(video_list[j][i]);
+        video_sinker = VideoSinker(instance_video_list);
         video_sinker.calculate_lock_step();
-        video_sinker.trimming_video_by_lock_step();
+        #video_sinker.trimming_video_by_lock_step(hard_offset[hard_offset_index]);
+        hard_offset_index += 1;
+    """
+
+    clip = VideoFileClip("./origin/_reo_241/0_4683_reo_241.mp4");
+    cv_clip = cv2.VideoCapture("./origin/_reo_241/0_4683_reo_241.mp4");
+
+    clip2 = VideoFileClip("./origin/_usb_normal/0_4683_usb_normal.mp4");
+    cv_clip2 = cv2.VideoCapture("./origin/_usb_normal/0_4683_usb_normal.mp4");
+    
+    print(cv_clip.get(cv2.CAP_PROP_FPS));
+    #print(clip.fps);
+    #new_clip = clip.set_fps(cv_clip.get(cv2.CAP_PROP_FPS));
+    #new_clip = clip.subclip(13.30023860931396484375, 20.0);
+    #new_clip.write_videofile("./test.mp4", fps=cv_clip.get(cv2.CAP_PROP_FPS));
+
+    newclip2= clip2.subclip(10.0, 20.0);
+    newclip2.write_videofile("./test2.mp4", fps=cv_clip2.get(cv2.CAP_PROP_FPS), codec="mjpeg", preset="ultrafast", bitrate="75000K");
+    #ffmpeg_extract_subclip("./origin/_reo_241/0_4647_reo_241.mp4", 4.3, float(20.0), targetname="./test.mp4");
+
+    """
